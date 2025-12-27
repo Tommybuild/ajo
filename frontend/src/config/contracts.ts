f2175d78e0dc61c7a8c93a93ace926b684cdc90f
Oluwatomilola <tommy4gold@gmail.com>
2025-12-27 11:04:12 +0100

    chore(frontend): update ABI to match PiggyBank withdraw() signature

--- frontend/src/config/contracts.ts
+++ frontend/src/config/contracts.ts
@@ -14,15 +14,8 @@ export const PIGGYBANK_ABI = [
     type: 'function'
   },
   {
-    inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
-    name: 'withdraw',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
     inputs: [],
-    name: 'withdrawAll',
+    name: 'withdraw',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function'
